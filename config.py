295bbd24444072c1aa7234fb027cdf16736946a4
glenn <glenn@gjoneslaptop.(none)>
2009-04-01 19:36:00 +0100

    Initial customisations and fixes

--- config.py
+++ config.py
@@ -15,13 +15,13 @@ BLOG = {
     "bloog_version": "0.8",
     "html_type": "text/html",
     "charset": "utf-8",
-    "title": "Bloog",
-    "author": "Bill Katz",
+    "title": "Millennium Hand",
+    "author": "Glenn Jones",
     # This must be the email address of a registered administrator for the 
     # application due to mail api restrictions.
-    "email": "you@foo.com",
-    "description": "A RESTful Blog/Homepage for Google AppEngine.",
-    "root_url": "http://bloog.billkatz.com",
+    "email": "glenn@millenniumhand.co.uk",
+    "description": "",
+    "root_url": "http://millenniumhand.co.uk",
     "master_atom_url": "/feeds/atom.xml",
     # By default, visitors can comment on article for this many days.
     # This can be overridden by setting article.allow_comments
@@ -65,42 +65,21 @@ PAGE = {
           "url": "/contact"},
     ],
     "featuredMyPages": {
-        "title": "Bloog Development",
-        "description": "Get involved",
+        "title": "Blogs",
+        "description": "",
         "entries": [
-            { "title": "Source Code", 
-              "url": "http://github.com/DocSavage/bloog", 
-              "description": "GitHub repository" },
-            { "title": "Tarball", 
-              "url": "http://github.com/DocSavage/bloog/tarball/master", 
-              "description": "Most recent snapshot" },
-            { "title": "Group", 
-              "url": "http://groups.google.com/group/bloog/topics", 
-              "description": "Developer discussion" },
-            { "title": "Author's Bloog", 
-              "url": "http://www.billkatz.com", 
-              "description": "What's brewing" },
-            { "title": "Architecture Diagram", 
-              "url": "/static/images/architecture2.png", 
-              "description": "RESTful Bloog" }
         ]
     },
     "featuredOthersPages": {
-        "title": "Google App Engine",
-        "description": "Developer Resources",
+        "title": "Links",
+        "description": "",
         "entries": [
+            { "title": "Bloog at GitHub", 
+              "url": "http://github.com/DocSavage/bloog", 
+              "description": "GitHub repository" },
             { "title": "Google App Engine", 
               "url": "http://code.google.com/appengine/", 
               "description": "The mothership" },
-            { "title": "App Engine Group", 
-              "url": "http://groups.google.com/group/google-appengine", 
-              "description": "Developer group" },
-            { "title": "App Engine Open Source", 
-              "url": "http://groups.google.com/group/google-appengine/web/google-app-engine-open-source-projects", 
-              "description": "Code!" },
-            { "title": "App Engine Console", 
-              "url": "http://appengine.google.com", 
-              "description": "Your apps" }
         ]
     },
 }
